d34c44a004cd16aec5df516de883de48f608591f
gaddamchandrakanth <gaddamchandrakanth1995@gmail.com>
2025-07-02 16:15:23 +0530

    Create test.groovy

---
+++ vars/test.groovy
@@ -0,0 +1,16 @@
+def sum(a,b)
+{
+  echo "The sum of ${a} and ${b} is ${a+b}"
+}
+def sub(a,b)
+{
+  echo "The subtraction of ${a} and ${b} is ${a-b}"
+}
+def mul(a,b)
+{
+  echo "The multiply of ${a} and ${b} is ${a*b}"
+}
+def div(a,b) 
+{
+  echo "The divide of ${a} and ${b} is ${a/b}"
+}
