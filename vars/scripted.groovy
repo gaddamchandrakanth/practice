25aba83d03da4ff3cd23ad8151d6ee18ec6d0179
gaddamchandrakanth <gaddamchandrakanth1995@gmail.com>
2025-07-02 16:47:45 +0530

    Update scripted.groovy

--- vars/scripted.groovy
+++ vars/scripted.groovy
@@ -6,5 +6,7 @@ def build()
 {
   sh "mvn package"
 }
-
+def deploymentartifact(jobname,IP,contextpath)
+{
+  sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${IP}:/var/lib/tomcat10/webapps/${contextpath}.war"
   
